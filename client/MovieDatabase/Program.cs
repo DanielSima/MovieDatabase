90d26fd30bec6087931c75f9392c8e6ea6994db8
Daniel Šíma <dan0nik192000@gmail.com>
2020-01-02 17:18:49 +0100

    fixes

--- client/MovieDatabase/Program.cs
+++ client/MovieDatabase/Program.cs
@@ -9,6 +9,10 @@ namespace MovieDatabase
         public static void Main(string[] args)
         {
             //Used to import data, in future could let user add movie by name or something.
+			
+			//If you want to test the application, import data from sql. This code runs more than 45
+			// minutes because of api limitation.
+			
             //ImportFromTMDB.ImportAll(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
 
             CreateHostBuilder(args).Build().Run();
